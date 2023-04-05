8a9ccbc6f9244a830b37c25ea67cf93b51a7419c
TatianaChepovaya <taniachepovaya@gmail.com>
2023-04-05 12:20:12 +0300

    ,

--- index.js
+++ index.js
@@ -40,9 +40,60 @@ $(document).ready(function () {
     dots: true,
   })
 })
+$(document).ready(function () {
+  $('.graduates__slider').slick({
+    slidesToShow: 3,
+    rows:2,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
 
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+        },
+      },
+    ],
+  })
+})
 $(document).ready(function ($) {
   if ($(window).width() >= 1024) {
     $('.statistics__slider').slick('unslick')
   }
+})
+$(document).ready(function () {
+  $('.preview__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          centerMode: true,
+          centerPadding: '40px',
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
 })
