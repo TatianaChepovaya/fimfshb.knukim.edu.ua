2a250be3001976bcb5cf502381f61267c64ab05d
TatianaChepovaya <taniacheport@gmail.com>
2023-04-07 09:50:45 +0300

    ,

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.statistics__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: false,
     dots: true,
@@ -43,23 +43,24 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.graduates__slider').slick({
     slidesToShow: 3,
-    rows:2,
+    rows: 2,
     slidesToScroll: 3,
     autoplay: false,
     autoplaySpeed: 2000,
     arrows: false,
-    
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-
         },
       },
       {
         breakpoint: 768,
         settings: {
+          rows: 1,
+          slidesPerRow: 1,
+          slidesToScroll: 1,
           slidesToShow: 1,
           dots: true,
         },
@@ -76,21 +77,111 @@ $(document).ready(function () {
   $('.preview__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: false,
     responsive: [
       {
+        breakpoint: 1330,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          centerMode: true,
-          centerPadding: '40px',
         },
       },
       {
         breakpoint: 768,
         settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
+})
+
+$(document).ready(function () {
+  $('.courses__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1330,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          rows: 3,
+          slidesToScroll: 1,
+          slidesPerRow: 1,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
+})
+
+$(document).ready(function () {
+  if ($(window).width() < 768) {
+    $('.courses__arrow-btn').appendTo('.courses__left-part')
+  }
+})
+
+$(document).ready(function () {
+  $('.news__slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+
+    responsive: [
+      {
+        breakpoint: 1330,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          dots: true,
+        },
+      },
+    ],
+  })
+})
+$(document).ready(function () {
+  $('.direction__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToScroll: 1,
           slidesToShow: 1,
         },
       },
