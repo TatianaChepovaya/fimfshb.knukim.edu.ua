407da499c95eff66a876d573791549b96a64cbb6
TatianaChepovaya <taniacheport@gmail.com>
2023-04-08 15:06:15 +0300

    .

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ $(document).ready(function () {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
+          rows: 1,
+          slidesPerRow: 1,
+          slidesToScroll: 1,
+          dots: true,
         },
       },
       {
